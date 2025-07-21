d28670cfe24501d53917b9ef6ce36291f894838b
王海槟 <wanghaibin@wanghaibins-MacBook-Air.local>
2025-07-21 18:27:21 +0800

    feat: create fiber

--- index.jsx
+++ index.jsx
@@ -49,23 +49,88 @@ function createTextElement(text) {
   };
 }
 
-// render函数 - 将element渲染到container dom内
-function render(element, container) {
-  // create new dom from element
+// 根据fiber创建&返回dom节点
+function createDom(fiber) {
+  // create new dom from fiber
   const dom =
-    element.type == "TEXT_ELEMENT" // 判断是否是文本节点
+    fiber.type == "TEXT_ELEMENT" // 判断是否是文本节点
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
   // add dom props
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-  // add children recursively
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+  return dom;
+}
+
+function render(element, container) { 
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+// 下次要执行的渲染任务
+let nextUnitOfWork = null;
+// 将整个渲染过程从同步改成分片防止阻塞主线程
+function workLoop(deadline) { 
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) { 
+  // 1. add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // 2. create new fibers - children fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) { 
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // 3. return next unit of work - child -> sibling -> uncle(parent's silbling)
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) { 
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 // my React for stydy
